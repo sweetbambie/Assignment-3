541ddab1945909abdf04b9204a97df4b4716e5d2
sweetbambie <sophia@miao.fm>
2024-11-01 02:30:51 -0400

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,36 +1,27 @@
-
-function cosineLaw () {
-
-    if(event) {
-        event.preventDefault()
-    }
-
-    var a = parseFloat(document.getElementById ('sideA').value);
-    var b = parseFloat(document.getElementById ('sideB').value);
-    var c = parseFloat(document.getElementById ('angleC').value);
+function cosineLaw() {
+    var a = parseFloat(document.getElementById('sideA').value);
+    var b = parseFloat(document.getElementById('sideB').value);
+    var c = parseFloat(document.getElementById('angleC').value);
 
     if (isNaN(a) || isNaN(b) || isNaN(c)) {
         alert("Please enter valid numbers for Side A, Side B, and Angle C.");
-        return; 
-
+        return;
     }
 
-    var angleCRadians = c * (Math.PI /180)
+    var angleCRadians = c * (Math.PI / 180);
+    var sideC = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) - 2 * a * b * Math.cos(angleCRadians));
 
-    var cSquared = Math.pow(a, 2) + Math.pow(b, 2) - 2 * a * b * Math.cos(angleCRadians);
-    var c = Math.sqrt(cSquared);
-
-    document.getElementById('sideC').value = c.toFixed(2);
+    document.getElementById('sideC').value = sideC.toFixed(2);
 }
 
+function asymptoteFinder() {
+    var m = parseFloat(document.getElementById('mvalue').value);
+    var n = parseFloat(document.getElementById('nvalue').value);
+    var output = "";
 
-
-
-
-function asymptoteFinder () {
-
-    if(event) {
-        event.preventDefault()
+    if (isNaN(m) || isNaN(n)) {
+        alert("Please enter valid numbers for n Value and m Value.");
+        return;
     }
 
     var m = parseFloat(document.getElementById ('mvalue').value);
@@ -84,6 +75,7 @@ function asymptoteFinder () {
             output ="Septic"
         }
 
+
         else if ((m-n) === 8){ 
             output ="Octic"
         }
@@ -95,33 +87,30 @@ function asymptoteFinder () {
         else if ((m-n) === 10){ 
             output ="Decic"
         }
+
+        else {
+
+            output ="Undefined"
+        }
     }
 
 
     document.getElementById('atype').value = output;
-
 }
 
-
-function piApprox (){
-
-    if(event) {
-        event.preventDefault()
-    }
-
+function piApprox() {
     let pi = 0;
+    var n = parseFloat(document.getElementById('Nvalue').value);
 
-    var n = parseFloat(document.getElementById ('aside').value);
+    // if (isNaN(n)) {
+    //     alert("Please enter a valid numbers for n Value.");
+    //     return;
+    // }
 
-    for (let i =0; i <= n; i++){
-
-        pi += ( Math.pow(-1, i) ) / (( 2*i ) + 1)
+    for (let i = 0; i <= n; i++) {
+        pi += (Math.pow(-1, i)) / ((2 * i) + 1);
     }
 
-    pi = pi * 4
-    
-
+    pi = pi * 4;
     document.getElementById('piapprox').value = pi;
-        
 }
-
